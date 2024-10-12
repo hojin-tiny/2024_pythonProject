a91657822685d539539ed440bb73f28d8ca73ce2
hojin-tiny <kms10567@gmail.com>
2024-10-12 11:50:35 +0900

    operator 예제 - 숫자,연산

--- practice/if_statement.py
+++ practice/if_statement.py
@@ -1,13 +1,41 @@
-size = int(input("수학 시험 점수를 입력해주쇼"))
+#size = int(input("수학 시험 점수를 입력해주쇼"))
+#print(size)
+
+#if size <= 100:
+    #if size >= 90:
+     #   print("당신은 상반입니다")
+  #  elif size >= 70:\
+   #     print("당신은 중반입니다")
+ #   else:
+#        print("당신은 하반입니다")
+#else:
+ #   print("잘못된 점수입니다")
+
+# 예제1번
+size = int(input("숫자를 입력해주세요"))
 print(size)
 
-if size <= 100:
-    if size >= 90:
-        print("당신은 상반입니다")
-    elif size >= 70:\
-        print("당신은 중반입니다")
-    else:
-        print("당신은 하반입니다")
+if size %2 = 0:
+    print("짝수입니다")
 else:
-    print("잘못된 점수입니다")
+    print("홀수입니다")
 
+
+#size = str(input("어떤 얘기를 해주세요"))
+#print(size)
+
+#if size = ("파이썬"):
+ #   print("파이썬 관련된 얘기군요")
+
+
+
+mbti2 = 0
+mbti2 = int(input("당신은 주말에 보통 무엇을 하며 시간을 보내나요? 1.집에서 쉬는 편이다. 2.친구들과 약속을 잡아 나가는 편이다."))
+print(mbti2)
+
+if mbti2 = 1:
+    mbti = I
+else mbti2 = 2:
+    mbti = E
+
+mbti2 = int(input("갑자기 상공에 외계인이 나타난다면? 1."))
