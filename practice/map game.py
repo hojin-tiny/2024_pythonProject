6bdeeea30c889cf010617ab91f0f01ddd6f63c88
hojin-tiny <kms10567@gmail.com>
2024-11-09 11:34:04 +0900

    .

---
+++ practice/map game.py
@@ -0,0 +1,18 @@
+x = 0
+y = 0
+(x,y)
+
+str(input"이동할 방향을 입력해주세요(UP,DOWN,RIGHT,LEFT")
+if str = "UP":
+    y = y + 1
+
+
+if str = "DOWN"
+    y = y - 1
+
+if str = "LEFT":
+    x = x - 1
+
+if str = "RIGHT":
+    x = x + 1
+
