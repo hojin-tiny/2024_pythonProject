ca8f60c98026c7aae9f5f820d1dd46ecc4cd009c
hojin-tiny <kms10567@gmail.com>
2025-01-04 12:10:54 +0900

    rename operator.py

--- practice/for-example-map.py
+++ practice/for-example-map.py
@@ -69,7 +69,7 @@ alphabet = []
 
 for i in range(0,10):
     alphabet.append(chr(random.randrange(65,91)))
-
+user_input = ""
 for alpha in alphabet:
     print(alpha)
     while user_input !=alpha:
